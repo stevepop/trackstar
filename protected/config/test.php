66e6fd6d8edc1cbe70de89b0368669911e1878c9
Steve Popoola <stevepop@gmail.com>
2011-06-17 11:54:15 +0100

    trackstar

---
+++ protected/config/test.php
@@ -0,0 +1,29 @@
+<?php
+
+return CMap::mergeArray(
+	require(dirname(__FILE__).'/main.php'),
+	array(
+		'components'=>array(
+			'fixture'=>array(
+				'class'=>'system.test.CDbFixtureManager',
+			),
+		       /*	'db'=>array(
+				'connectionString' => 'mysql:host=127.0.0.1;dbname=trackstar_test_2',
+				'emulatePrepare' => true,
+				'username' => 'root',
+				'password' => '',
+				'charset' => 'utf8',
+		     ),*/
+			 // uncomment the following to use a MySQL database
+
+                  'db'=>array(
+                        'connectionString' => 'mysql:host=localhost;dbname=trackstar_test',
+                        'emulatePrepare' => true,
+                        'username' => 'steve',
+                        'password' => 'stevedba',
+                        'charset' => 'utf8',
+                ),
+
+		),
+	)
+);
